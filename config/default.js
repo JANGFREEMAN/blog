1e2883ed883d8b5a4fa6b9d8b6f59f6160256e7e
unknown <>
2017-02-10 13:32:53 +0800

    update config

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
     key: 'myblog',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/myblog'
-  // mongodb: 'mongodb://zhangyx:freeman111@ds111529.mlab.com:11529/myblog'
+  // mongodb: 'mongodb://localhost:27017/myblog'
+  mongodb: 'mongodb://zhangyx:freeman111@ds111529.mlab.com:11529/myblog'
 };
